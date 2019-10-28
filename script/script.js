2f62fed21e8f6086ea462c88dd6804276c3463f5
Darren Zou <darrenzou1221@gmail.com>
2019-10-28 12:45:44 -0400

    added mobile check and get center coords

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
+var onmobile;
+ function mobilecheck() {
+   var check = false;
+(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+onmobile=check 
+};
+ mobilecheck()
 
 setCanvasSize(); 
 function loadlayer(location,layer){
-   lastloadx=posX;
-         lastloady=posY;
+   lastloadx=CenterCoord().x;
+         lastloady=CenterCoord().y;
    var data= {
       location:location,
       layer:layer
@@ -25,9 +32,6 @@ function loadlayer(location,layer){
 sendDataToLoad(jdata_location,jdata_imageurl,jdata_scale,jdata_level);
 ///
 var canvas = new fabric.Canvas('c');
-
-
-
 var zoom;
 var zoomlevel=1;
 var layeronearray=[];
@@ -136,8 +140,9 @@ if(layertwoarray.length!=0&&layertwoarray[0].opacity!=1){
 
 }
 //layer 3 turn visible
-if(layerthreearray[0].opacity!=0){
+
   if(zoomlevel>=90){
+     if(layerthreearray[0].opacity!=0){
    for(var i=0;i<layeronearray.length;i++){
       layeronearray[i].opacity=0;
       }
@@ -173,7 +178,6 @@ function addtoarray(image,level){
 function getCurrentCordinates(posX,posY,level){
  if(level==1){
     widthPerImage=75*perpixelX
- 
     heightPerImage=75*perpixelY
    x=Math.floor((posX/widthPerImage)%16)+1;//x is fine
    y=Math.floor(posY/heightPerImage);
@@ -195,6 +199,8 @@ function getCurrentCordinates(posX,posY,level){
   return y*1200+x;
    }
 }
+
+
 
 //MOVEMENTS AND BUTTONS AND INFOMATION DISPLAYS
 $('#show').click( function(){
@@ -219,23 +225,64 @@ $("#reset").click( function()
         canvas.viewportTransform[5]=0;
          canvas.requestRenderAll();
          document.getElementById('zoomlevel').innerHTML="zoomlevel "+ zoomlevel;
+         
+
          }
       );
+      //mobile control
+      canvas.on({
+         'touch:gesture': function(e) {
+             if (e.e.touches && e.e.touches.length == 2) {
+                 pausePanning = true;
+                 var point = new fabric.Point(e.self.x, e.self.y);
+                 if (e.self.state == "start") {
+                     zoomStartScale = canvas.getZoom();
+                 }
+                 var delta = zoomStartScale * e.self.scale;
+                 canvas.zoomToPoint(point, delta);
+                 pausePanning = false;
+             }
+         },
+         'object:selected': function() {
+             pausePanning = true;
+         },
+         'selection:cleared': function() {
+             pausePanning = false;
+         },
+         'touch:drag': function(e) {
+             if (pausePanning == false && undefined != e.self.x && undefined != e.self.x) {
+                 currentX = e.self.x;
+                 currentY = e.self.y;
+                 xChange = currentX - lastX;
+                 yChange = currentY - lastY;
+     
+                 if( (Math.abs(currentX - lastX) <= 50) && (Math.abs(currentY - lastY) <= 50)) {
+                     var delta = new fabric.Point(xChange, yChange);
+                     canvas.relativePan(delta);
+                 }
+     
+                 lastX = e.self.x;
+                 lastY = e.self.y;
+             }
+         }
+     });
+     
 var panning = false;
 canvas.on('mouse:down', function(e) {
 panning = true;
+this.isDragging=true;
 });
 
 canvas.on('mouse:up', function(e) {
 panning = false;
-
 });
-
 canvas.on('mouse:move', function(e) {
-canvas.selection = false;
-if (panning && e && e.e) {
- var delta = new fabric.Point(e.e.movementX, e.e.movementY);
- canvas.relativePan(delta);
+   canvas.selection = false;
+   if (panning && e && e.e) {
+   var delta = new fabric.Point(e.e.movementX, e.e.movementY);
+   canvas.relativePan(delta);
+   console.log('x',CenterCoord().x);
+   console.log('y',CenterCoord().y);
 }
 });
 
@@ -258,31 +305,17 @@ opt.e.preventDefault();
 opt.e.stopPropagation();
 document.getElementById('zoomlevel').innerHTML="zoomlevel "+ zoomlevel;
 if(zoomlevel==15){
-   loadlayer(getCurrentCordinates(posX,posY,2),2);
+   loadlayer(getCurrentCordinates(CenterCoord.x,CenterCoord.y,2),2);
 }
 if(zoomlevel==75){
-   loadlayer(getCurrentCordinates(posX,posY,3),3)
+   loadlayer(getCurrentCordinates(CenterCoord.x,CenterCoord.y,3),3)
 }
 changelayers();
 });
 
 
-canvas.on('mouse:down', function(opt) {
-var evt = opt.e;
- this.isDragging = true;
- this.lastPosX = evt.clientX;
- this.lastPosY = evt.clientY;
-
-});
 canvas.on('mouse:move', function(opt) {
-if (this.isDragging) {
- var e = opt.e;
- this.viewportTransform[4] += e.clientX - this.lastPosX;
- this.viewportTransform[5] += e.clientY - this.lastPosY;
- this.requestRenderAll();
- this.lastPosX = e.clientX;
- this.lastPosY = e.clientY;
-}  
+
 /// loads images at second layer
 if(zoomlevel>20&&zoomlevel<90){
    differenceinX=Math.abs(lastloadx-posX);
@@ -362,8 +395,6 @@ function imageInfoPage(imgUrl,url,description){
 }
 
 
-
-
 function getzoomlevel(delta){
 if(delta<0){
 zoomlevel-=1;
@@ -376,7 +407,7 @@ zoomlevel+=1;
 }
 
 //this function determines window size and therefore, screen size
-function setCanvasSize() {
+function setCanvasSize(){
    var myWidth = 0, myHeight = 0;
 if( typeof( window.innerWidth ) == 'number' ) {
   //Non-IE
@@ -405,4 +436,12 @@ $('#pop').css({'display':'none'});
 $('#overlay').css({'display':'none'});
 });
 
+function CenterCoord(){
+  
+   return{
+      x:fabric.util.invertTransform(canvas.viewportTransform)[4]+(canvas.width/zoom)/2,
+      y:fabric.util.invertTransform(canvas.viewportTransform)[5]+(canvas.height/zoom)/2
+   }
+
+}
 
