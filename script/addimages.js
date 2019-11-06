78eaf3bf876563028b0629ea89a381487e17365d
Darren Zou <darrenzou1221@gmail.com>
2019-11-05 23:14:30 -0500

    fixed boxing system

--- script/addimages.js
+++ script/addimages.js
@@ -1,35 +1,34 @@
 makelist(1,13)
 function makelist(l1,l2){
-squarelength=4;
+
 var locationlist=[];
 var topcorner= Math.max(l1,l2);
 var bottomcorner=Math.min(l1,l2);
-var height =Math.floor(topcorner/4)-Math.floor(bottomcorner/4)+1
+var height =Math.floor(topcorner/1200)-Math.floor(bottomcorner/1200)+1
 var bottomleft,bottomright
-if((topcorner-(height-1)*4)<bottomcorner){
-    bottomleft=topcorner-(height-1)*4 
+if((topcorner-(height-1)*1200)<bottomcorner){
+    bottomleft=topcorner-(height-1)*1200 
    bottomright=bottomcorner
    }else{
        bottomleft= bottomcorner
-   bottomright=topcorner-(height-1)*4
+   bottomright=topcorner-(height-1)*1200
    }
 
 for(var i=bottomleft;i<=bottomright;i++){
 locationlist.push(i)
     for(var j=1;j<height;j++){
-    locationlist.push(i+j*4)
+    locationlist.push(i+j*1200)
     }
-    console.log(locationlist)
+//     console.log(locationlist)
 }
 console.log('height',height)
 console.log(bottomleft,bottomright)
 
-// var imageurl=['/images/greensquare.png']
-// for(var i=0;i<locationlist.length;i++){
-//     console.log('this is running')
-//     sendDataToLoad(locationlist,imageurl,4,4)
+var imageurl=['/images/greensquare.png']
+for(var i=0;i<locationlist.length;i++){
+    sendDataToLoad(locationlist,imageurl,1200,1200)
     
-// }
+}
 }
 
 
