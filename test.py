efc6d9a69a2d1168e7ef439b56f4862e1e3931ab
Xatnagh <50595881+Xatnagh@users.noreply.github.com>
2020-02-13 17:24:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from google.appengine.ext import ndb
 from google.appengine.api import images
 from Images import ImageInfo
 def test():
-    a=ImageInfo.query(ImageInfo.location>=2, ImageInfo.location<=1203,ImageInfo.level==3).fetch()
-    # put every condition in separate commas
-    for image in a:
-        print(image.location)
+    print('a')
+#     a=ImageInfo.query(ImageInfo.location>=2, ImageInfo.location<=1203,ImageInfo.level==3).fetch()
+#     # put every condition in separate commas
+#     for image in a:
+#         print(image.location)
